6d4a530cd902839536ce0d7143e24a1dcd67f06b
danielOuattara <daniel.ouattara@gmx.com>
2023-12-19 22:44:33 +0100

    12 Counter + passing down setState and children

--- _12_React_Typescript_Course/src/App.tsx
+++ _12_React_Typescript_Course/src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
 import { Section } from "./components/Section";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <Heading title="React + TypeScript" />
@@ -10,7 +13,9 @@ export default function App() {
         <p>This is the child JSX for Section component</p>
         <p>Me too ! </p>
       </Section>
-      <Counter />
+      <Counter count={count} setCount={setCount}>
+        <h2>The count children value is : {count}</h2>
+      </Counter>
     </>
   );
 }
--- _12_React_Typescript_Course/src/components/Counter.tsx
+++ _12_React_Typescript_Course/src/components/Counter.tsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function Counter() {
-  //   const [count, setCount] = useState<number>(0); // OK!
-  // OR
-  const [count, setCount] = useState(0);
+// export default function Counter() {
+//   //   const [count, setCount] = useState<number>(0); // OK!
+//   // OR
+//   const [count, setCount] = useState(0);
+//   return (
+//     <>
+//       <h1>Count is : {count}</h1>
+//       <button onClick={() => setCount((prevState) => prevState - 1)}>-</button>
+//       <button onClick={() => setCount(0)}>
+//         Reset
+//       </button>
+//       <button onClick={() => setCount((prevState) => prevState + 1)}>+</button>
+//     </>
+//   );
+// }
+
+//--------------------------------------------------------------------------------
+// State in App.tsx, no more here !
+//
+
+import { ReactNode } from "react";
+
+type CounterType = {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+  children: ReactNode;
+};
+
+export default function Counter({ count, setCount, children }: CounterType) {
   return (
     <>
-      <h1>Count is : {count}</h1>
+      <h1>Count value is : {count}</h1>
+      {children}
       <button onClick={() => setCount((prevState) => prevState - 1)}>-</button>
-      <button onClick={() => setCount(0)}>
-        Reset
-      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
       <button onClick={() => setCount((prevState) => prevState + 1)}>+</button>
     </>
   );
