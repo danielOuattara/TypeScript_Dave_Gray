e4c1de104ffa951ec7becc08be09a3907e842c51
danielOuattara <daniel.ouattara@gmx.com>
2023-12-20 00:00:19 +0100

    12 Generic List component

--- _12_React_Typescript_Course/src/App.tsx
+++ _12_React_Typescript_Course/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
 import { Section } from "./components/Section";
+import List from "./components/List";
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +17,25 @@ export default function App() {
       <Counter count={count} setCount={setCount}>
         <h2>The count children value is : {count}</h2>
       </Counter>
+
+      <List itemsList={["one", "two", "three"]} />
+
+      <List
+        itemsList={[1, 2, 3]}
+        render={(item: number) => <span className="gold">{item}</span>}
+      />
+
+      <List
+        itemsList={[
+          { name: "John", country: "Jamaica" },
+          { name: "Jana", country: "Bolivia" },
+        ]}
+        render={(item: { name: string; country: string }) => (
+          <span className="gold">
+            My name is {item.name}. I come from {item.country}
+          </span>
+        )}
+      />
     </>
   );
 }
