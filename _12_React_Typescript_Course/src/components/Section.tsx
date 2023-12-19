74a49d742ef6e3024b28d5d06d7003a800bc7165
danielOuattara <daniel.ouattara@gmx.com>
2023-12-19 22:33:43 +0100

    12 Section: recommended code + Default values

--- _12_React_Typescript_Course/src/components/Section.tsx
+++ _12_React_Typescript_Course/src/components/Section.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+// OLD way
 
-export const Section: React.FC<{ title: string }> = ({ children, title }) => {
+// import React from "react";
+
+// export const Section: React.FC<{ title: string }> = ({ children, title }) => {
+//   return (
+//     <section>
+//       <h2>{title}</h2>
+//       {children}
+//     </section>
+//   );
+// };
+
+//---------------------------------------------------------------------------------
+
+// RECENT
+import { ReactNode } from "react";
+
+type SectionType = {
+  title?: string;
+  children: ReactNode;
+};
+
+export const Section = ({
+  children,
+  title = "My Heading from Section",
+}: SectionType) => {
   return (
     <section>
       <h2>{title}</h2>
