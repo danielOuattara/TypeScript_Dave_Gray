f93d4c2d29dacb8c364998031a79ecd2220f3954
danielOuattara <daniel.ouattara@gmx.com>
2023-12-20 12:40:29 +0100

    14 useCallback hook

--- _14_TypeScript_and_React_Hooks/src/App.tsx
+++ _14_TypeScript_and_React_Hooks/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import "./App.css";
+import { useState, useEffect, useCallback, Key } from "react";
 
 type User = {
   id: number;
@@ -9,8 +10,8 @@ function App() {
   // const [data, setData] = useState(0); // Correct
   // const [data, setData] = useState<number | null>(0); // Correct
   // const [data, setData] = useState<null | User>(null); // Correct
+  const [counter, setCounter] = useState<number>(0); // Correct
   const [users, setUsers] = useState<User[] | null>([]); // Correct
-  const [data, setData] = useState<number>(0); // Correct
 
   //----
   // side effect: One thing change and another thing also changes because of the 1st change
@@ -34,7 +35,29 @@ function App() {
 
   //
 
-  return <div className="App"></div>;
+  const adder = useCallback(
+    (
+      event: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>,
+    ) => {
+      setCounter((prevState) => prevState + 1);
+    },
+    [],
+  );
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setCounter((prevState) => prevState - 1);
+        }}
+      >
+        -
+      </button>
+
+      <button onClick={adder}>+</button>
+      <h1>{counter}</h1>
+    </div>
+  );
 }
 
 export default App;
